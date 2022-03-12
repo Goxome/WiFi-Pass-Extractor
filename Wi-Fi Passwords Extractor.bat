5a06df0a1e03063074d0b5df15378477abf8bf5e
Goxome <56850970+Goxome@users.noreply.github.com>
2022-03-12 14:31:53 +0530

    Add files via upload

---
+++ Wi-Fi Passwords Extractor.bat
@@ -0,0 +1,20 @@
+@echo off
+title Wi-fi Extractor
+mkdir %userprofile%\Desktop\Wi-Fi_Passwords
+netsh wlan export profile folder=%userprofile%\Desktop\Wi-Fi_Passwords key=clear
+cls
+echo [32m
+echo.
+echo.
+echo all Wi-Fi details have been exported to Desktop\Wi-Fi_Passwords
+echo.
+echo [34m Social Media
+echo.
+echo [35mYouTube:[96m www.youtube.com/c/Goxome/
+echo.
+echo.
+echo [33m Please Subscribe To My Channel on YouTube ;)
+echo [96m
+PAUSE
+PAUSE
+Start https://youtube.com/c/Goxome
